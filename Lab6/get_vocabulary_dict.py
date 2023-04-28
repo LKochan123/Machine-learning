cb403a7fee4dba7b9ca5f44513c84b286b219977
LKochan123 <kochanczyk.lukasz@gmail.com>
2023-04-28 12:50:57 +0200

    Final commit

---
+++ Lab6/get_vocabulary_dict.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+
+from typing import Dict
+
+
+def get_vocabulary_dict() -> Dict[int, str]:
+    """Read the fixed vocabulary list from the datafile and return.
+
+    :return: a dictionary of words mapped to their indexes
+    """
+
+    # FIXME: Parse data from the 'data/vocab.txt' file.
+    # - The file is saved in tab-separated values (TSV) format.
+    # - Each line contains a word's ID and the word itself.
+    # The output dictionary should map word's ID on the word itself, e.g.:
+    #   {1: 'aa', 2: 'ab', ...}
+    word_dct = {}
+
+    with open('data/vocab.txt', 'r') as f:
+        for line in f:
+            id, word = line.strip().split('\t')
+            word_dct[int(id)] = word
+
+    return word_dct
